30dee7bee28155bd869a07a552edc7fa4174a81b
Aakash Harpalani <aharpal@toronto.ca>
2016-12-01 19:01:12 -0500

    Function for determining road direction (in TO)
    Function takes angle between start and end points of corridor and
    identifies corridor as E/W, N/S, or DIAG

---
+++ sql/create-function-direction.sql
@@ -0,0 +1,18 @@
+﻿CREATE OR REPLACE FUNCTION direction(angle double precision)
+	RETURNS TEXT AS $$
+DECLARE
+	result refcursor;
+BEGIN
+	angle := angle + 17.5;
+	result := '';
+	IF (angle >= 60 AND angle <= 120) OR (angle >= 240 AND angle <= 300) THEN
+		result := 'E/W';
+	ELSIF (angle >= 150 AND angle <= 210) OR angle >= 330 OR angle <= 30 THEN
+		result := 'N/S';
+	ELSE
+		result := 'DIAG';
+	END IF;
+	RETURN result;		
+END;
+$$
+LANGUAGE 'plpgsql' IMMUTABLE;
