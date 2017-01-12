52379800c7c1abb7a72aa5085fa7b4065700ab33
Aakash Harpalani <aharpal@toronto.ca>
2017-01-12 17:26:19 -0500

    Create table centreline_volumes
    Final table for volume-centreline linkages

---
+++ sql/create-table-centreline_volumes.sql
@@ -0,0 +1,19 @@
+﻿DROP TABLE IF EXISTS prj_volume.centreline_volumes;
+
+CREATE TABLE prj_volume.centreline_volumes (
+	volume_id serial NOT NULL,
+	centreline_id int NOT NULL,
+	dir bit NOT NULL, --0 for "NE", 1 for "SW"
+	count_bin timestamp without time zone,
+	volume smallint,
+	count_type smallint
+	);
+
+INSERT INTO prj_volume.centreline_volumes( centreline_id, dir, count_bin, volume, count_type)
+SELECT	A.centreline_id
+
+FROM prj_volume.artery_tcl A
+INNER JOIN prj_volume.centreline B USING (centreline_id)
+
+
+
