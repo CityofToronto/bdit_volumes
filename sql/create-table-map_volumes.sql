5a59497e4fcc17ee0a18f7006ebe2efe7d6720d6
Aakash Harpalani <aharpal@toronto.ca>
2017-01-20 11:33:57 -0500

    Create Draft QGIS Volume Maps
    `map_volumes` - Feeds into QGIS mapping
    `test_map.qgs` - Volume Map for 2010 - 2016

---
+++ sql/create-table-map_volumes.sql
@@ -0,0 +1,20 @@
+﻿DROP TABLE IF EXISTS prj_volume.map_volumes;
+DROP TABLE IF EXISTS prj_volume.map_volumes_tmc;
+
+CREATE TABLE prj_volume.map_volumes(
+	centreline_id bigint,
+	dir_bin smallint,
+	opp_digitization smallint, 
+	daily_volume numeric,
+	num_days numeric,
+	count_type smallint
+);
+
+CREATE TABLE prj_volume.map_volumes_tmc(
+	centreline_id bigint,
+	dir_bin smallint,
+	opp_digitization smallint, 
+	daily_volume numeric,
+	num_days numeric,
+	count_type smallint
+);
---
+++ sql/update-table-map_volumes.sql
@@ -0,0 +1,20 @@
+﻿/*
+TRUNCATE prj_volume.map_volumes;
+TRUNCATE prj_volume.map_volumes_tmc;
+
+INSERT INTO prj_volume.map_volumes
+SELECT A.centreline_id, A.dir_bin, A.dir_bin * dir_binary((ST_Azimuth(ST_StartPoint(B.shape), ST_EndPoint(B.shape))+0.292)*180/pi()) AS opp_digitization, SUM(A.volume)/COUNT(A.count_bin)*(CASE WHEN A.count_type = 1 THEN 96 ELSE 32 END)*1.0 AS daily_volume, COUNT(A.count_bin)/24/4*1.0 AS num_days, A.count_type
+FROM prj_volume.centreline_volumes A
+INNER JOIN prj_volume.centreline B USING (centreline_id)
+WHERE A.count_bin >= '2010-01-01' AND A.count_bin < '2017-01-01' AND A.count_type = 1
+GROUP BY A.centreline_id, A.dir_bin,  A.dir_bin * dir_binary((ST_Azimuth(ST_StartPoint(B.shape), ST_EndPoint(B.shape))+0.292)*180/pi()), A.count_type
+ORDER BY A.centreline_id, A.dir_bin;
+*/
+
+INSERT INTO prj_volume.map_volumes_tmc
+SELECT A.centreline_id, A.dir_bin, A.dir_bin * dir_binary((ST_Azimuth(ST_StartPoint(B.shape), ST_EndPoint(B.shape))+0.292)*180/pi()) AS opp_digitization, SUM(A.volume)/COUNT(A.count_bin)*32*1.0 AS daily_volume, COUNT(A.count_bin)/24/4*1.0 AS num_days, A.count_type
+FROM (SELECT * FROM prj_volume.centreline_volumes WHERE count_type = 2) A
+INNER JOIN prj_volume.centreline B USING (centreline_id)
+WHERE A.count_bin >= '2010-01-01' AND A.count_bin < '2017-01-01'
+GROUP BY A.centreline_id, A.dir_bin,  A.dir_bin * dir_binary((ST_Azimuth(ST_StartPoint(B.shape), ST_EndPoint(B.shape))+0.292)*180/pi()), A.count_type
+ORDER BY A.centreline_id, A.dir_bin;
