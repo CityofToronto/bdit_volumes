690aa2cf5bc42447c8e6371b9ce1f261b04b0701
Aakash Harpalani <aharpal@toronto.ca>
2017-01-31 14:34:03 -0500

    Queries for generating count summaries

---
+++ sql/create-table-count_summary.sql
@@ -0,0 +1,21 @@
+﻿DROP TABLE IF EXISTS prj_volume.count_summary;
+
+CREATE TABLE prj_volume.count_summary(
+	arterycode bigint,
+	count_type character varying(10),
+	loc character varying(65),
+	count_date date,
+	source character varying(50),
+	category_id smallint);
+
+INSERT INTO prj_volume.count_summary
+SELECT 	A.arterycode,
+	A.count_type,
+	A.location as loc,
+	B.count_date, 
+	B.source1 as source,
+	B.category_id
+	
+FROM traffic.arterydata A
+INNER JOIN traffic.countinfo B USING (arterycode)
+ORDER BY A.arterycode, B.count_date;
---
+++ sql/create-table-count_summary_tmc.sql
@@ -0,0 +1,19 @@
+﻿DROP TABLE IF EXISTS prj_volume.count_summary_tmc;
+
+CREATE TABLE prj_volume.count_summary_tmc(
+	arterycode bigint,
+	count_type character varying(10),
+	loc character varying(65),
+	count_date date,
+	category_id smallint);
+
+INSERT INTO prj_volume.count_summary_tmc
+SELECT 	A.arterycode,
+	A.count_type,
+	A.location as loc,
+	B.count_date, 
+	B.category_id
+	
+FROM traffic.arterydata A
+INNER JOIN traffic.countinfomics B USING (arterycode)
+ORDER BY A.arterycode, B.count_date;
