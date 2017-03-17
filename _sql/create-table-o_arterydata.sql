66e8d6f5ee53893c2722b705f9b385962847ab6c
Aakash Harpalani <aharpal@toronto.ca>
2017-03-17 11:24:31 -0400

    Sample query for creating foreign tables
    Query connects to `arterydata` housed in FLOW oracle db

---
+++ _sql/create-table-o_arterydata.sql
@@ -0,0 +1,22 @@
+﻿DROP FOREIGN TABLE o_arterydata;
+CREATE FOREIGN TABLE o_arterydata
+(
+  arterycode bigint NOT NULL,
+  geomcode bigint,
+  street1 character varying(30),
+  street1type character varying(10),
+  street1dir character varying(5),
+  street2 character varying(30),
+  street2type character varying(10),
+  street2dir character varying(5),
+  street3 character varying(30),
+  street3type character varying(5),
+  street3dir character varying(5),
+  stat_code character varying(20),
+  count_type character varying(10),
+  location character varying(65),
+  apprdir character varying(10),
+  sideofint character varying(1),
+  linkid character varying(50),
+  seq_order character varying(7),
+  geo_id bigint) SERVER oradb OPTIONS (table 'ARTERYDATA');
