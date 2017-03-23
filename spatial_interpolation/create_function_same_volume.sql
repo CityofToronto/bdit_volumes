b959e5aefc4f4930110639df20e637ab1cf93151
sunnyqywang <sunnywang950529@hotmail.com>
2017-03-23 17:36:41 -0400

    Spatial Interpolation
    #24

---
+++ spatial_interpolation/create_function_same_volume.sql
@@ -0,0 +1,38 @@
+﻿DROP FUNCTION IF EXISTS same_volume(c1 bigint, c2 bigint);
+CREATE OR REPLACE FUNCTION same_volume(c1 bigint, c2 bigint)
+	RETURNS BOOLEAN AS $$
+DECLARE
+	NameNode boolean;
+	common_node bigint;
+	fc bigint;
+	result boolean;
+BEGIN
+	-- check if the two segments share a same node and road name and feature class
+	SELECT (t1.feature_code = t2.feature_code AND t1.linear_name_full = t2.linear_name_full) AND (ARRAY[t1.from_intersection_id, t1.to_intersection_id] && ARRAY[t2.from_intersection_id, t2.to_intersection_id]) INTO NameNode
+	FROM prj_volume.centreline t1, prj_volume.centreline t2
+	WHERE t1.centreline_id = c1 AND t2.centreline_id = c2;
+	
+	IF NameNode THEN 
+		SELECT feature_code INTO fc
+		FROM prj_volume.centreline
+		WHERE centreline_id = c1;
+		
+		SELECT (t1.from_intersection_id=t2.from_intersection_id)::int*t1.from_intersection_id +
+				(t1.from_intersection_id=t2.to_intersection_id)::int*t1.from_intersection_id +
+				(t1.to_intersection_id=t2.from_intersection_id)::int*t1.to_intersection_id +
+				(t1.to_intersection_id=t2.to_intersection_id)::int*t1.to_intersection_id INTO common_node
+		FROM prj_volume.centreline t1, prj_volume.centreline t2
+		WHERE t1.centreline_id = c1 AND t2.centreline_id = c2;
+		
+		SELECT NOT EXISTS(SELECT centreline_id
+				FROM prj_volume.centreline
+				WHERE (from_intersection_id = common_node OR to_intersection_id = common_node) AND (fc >= feature_code OR feature_code < 201401) AND (centreline_id not in (c1,c2))) INTO result;
+	ELSE
+		result := False;
+	END IF;
+	
+	RETURN result;
+		
+END;
+$$
+LANGUAGE 'plpgsql' IMMUTABLE;
---
+++ spatial_interpolation/group_centrelines.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 22 14:12:15 2017
+
+@author: qwang2
+"""
+
+import pandas as pd
+from pg import DB
+import configparser
+
+# CONNECTION SET UP
+CONFIG = configparser.ConfigParser()
+CONFIG.read('db.cfg')
+dbset = CONFIG['DBSETTINGS']
+
+db = DB(dbname=dbset['database'],host=dbset['host'],user=dbset['user'],passwd=dbset['password'])
+
+'''  
+pairs = pd.read_csv('pairs.csv', names = ['c1','c2','same'])
+pairs = pairs[pairs['same']=='t']
+
+root = list(set(list(pd.DataFrame(pairs['c1']).drop_duplicates()['c1'])+list(pd.DataFrame(pairs['c2']).drop_duplicates()['c2'])))
+to_visit = []
+visited = []
+chains = []
+while root:
+    current = root.pop()
+    visited.append(current)
+    to_visit.extend(list(pairs.groupby('c1').get_group(current)['c2']))
+    chain = [current]
+
+    while to_visit:
+        current = to_visit.pop()
+        if current not in visited:
+            chain.append(current)            
+            root.remove(current)
+            to_visit.extend(list(pairs.groupby('c1').get_group(current)['c2']))
+            visited.append(current)
+    
+    chains.append(chain)
+'''
+groups = {}
+count = 0
+table = []
+for group in chains:
+    for tcl in group:
+        table.append([tcl,count])
+    count = count + 1
+   
+tcl_no_merge = [x for t in db.query('SELECT centreline_id FROM prj_volume.centreline_groups RIGHT JOIN prj_volume.centreline USING (centreline_id) WHERE group_number is null and feature_code < 202000').getresult() for x in t]
+for tcl in tcl_no_merge:
+    table.append([tcl,count])
+    count = count + 1
+    
+db.truncate('prj_volume.centreline_groups')
+db.inserttable('prj_volume.centreline_groups',table)
