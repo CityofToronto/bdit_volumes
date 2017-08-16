16e985a4428fb72b62e2169cfc2d97c85b88100a
sunnyqywang <sunnywang950529@hotmail.com>
2017-08-16 10:37:11 -0400

    Add comments to sql
    #24

--- static_map/create_function_same_volume_minor_arterial.sql
+++ static_map/create_function_same_volume_minor_arterial.sql
@@ -1,5 +1,8 @@
-﻿DROP FUNCTION IF EXISTS prj_volume.same_volume_minor_arterial_group(g1 bigint, g2 bigint);
+﻿-- The function takes 2 group numbers from centreline_groups and returns a boolean indicating if they should be merged
+-- Merge if no intersecting minor arterial and above (excl. hwy b/c they are not at grade crossing)
+-- !!! Update centreline_groups_geom table if centreline_groups table is refreshed.
 
+DROP FUNCTION IF EXISTS prj_volume.same_volume_minor_arterial_group(g1 bigint, g2 bigint);
 CREATE OR REPLACE FUNCTION prj_volume.same_volume_minor_arterial_group(g1 bigint, g2 bigint)
 	RETURNS BOOLEAN AS $$
 DECLARE
@@ -17,6 +20,7 @@ BEGIN
 	FROM prj_volume.centreline_groups_geom t1, prj_volume.centreline_groups_geom t2
 	WHERE t1.group_number = g1 AND t2.group_number = g2;
 	
+	-- since the groups are directional, get the directions of the groups
 	SELECT dir_bin INTO dir1
 	FROM prj_volume.centreline_groups
 	WHERE group_number = g1;
@@ -24,7 +28,8 @@ BEGIN
 	SELECT dir_bin INTO dir2
 	FROM prj_volume.centreline_groups
 	WHERE group_number = g2;
-
+	
+	-- get the geometries
 	SELECT shape INTO geom1
 	FROM prj_volume.centreline_groups_geom
 	WHERE group_number = g1;
@@ -32,9 +37,11 @@ BEGIN
 	SELECT shape INTO geom2
 	FROM prj_volume.centreline_groups_geom
 	WHERE group_number = g2;
-
+	
+	-- return false if they are not the same direction
 	IF dir1 != dir2 THEN 
 		result := False;
+	-- continue if they share a common node
 	ELSIF NameNode THEN 
 		-- Find the common node
 		SELECT (CASE WHEN(t1.from_intersection=t2.from_intersection) THEN t1.from_intersection 
@@ -45,9 +52,11 @@ BEGIN
 		WHERE t1.group_number = g1 AND t2.group_number = g2;
 
 		-- True if no Minor Arterial + (not Expressway) is intersecting at the intersection
+		-- Intersecting segments -> segments share the common node but not parallel (within 15 degrees of g1 or g2)
 		SELECT NOT EXISTS(SELECT centreline_id
 				FROM prj_volume.centreline C JOIN prj_volume.centreline_groups G USING (centreline_id) 
 				WHERE (ST_AsText(ST_StartPoint(C.shape)) = common_node OR ST_AsText(ST_EndPoint(C.shape)) = common_node) AND (feature_code < 201301 AND feature_code!=201100) AND (group_number not in (g1,g2)) AND NOT (prj_volume.angle_diff(C.shape, geom1) < 15 OR prj_volume.angle_diff(C.shape, geom2) < 15)) INTO result;
+	-- return false if they do not share a common node
 	ELSE
 		result := False;
 	END IF;
--- volume_project/preprocessing/spatial_interpolation/create_function_same_volume.sql
+++ volume_project/preprocessing/spatial_interpolation/create_function_same_volume.sql
@@ -1,10 +1,10 @@
-﻿DROP FUNCTION IF EXISTS same_volume(c1 bigint, c2 bigint);
-CREATE OR REPLACE FUNCTION same_volume(c1 bigint, c2 bigint)
+﻿-- The function takes two centreline_ids and return a boolean value indicating whether they should be merged.
+DROP FUNCTION IF EXISTS prj_volume.same_volume(c1 bigint, c2 bigint);
+CREATE OR REPLACE FUNCTION prj_volume.same_volume(c1 bigint, c2 bigint)
 	RETURNS BOOLEAN AS $$
 DECLARE
 	NameNode boolean;
 	common_node bigint;
-	fc bigint;
 	result boolean;
 BEGIN
 	-- check if the two segments share a same node and road name and feature class
@@ -12,11 +12,10 @@ BEGIN
 	FROM prj_volume.centreline t1, prj_volume.centreline t2
 	WHERE t1.centreline_id = c1 AND t2.centreline_id = c2;
 	
-	IF NameNode THEN 
-		SELECT feature_code INTO fc
-		FROM prj_volume.centreline
-		WHERE centreline_id = c1;
+	-- continue checking if there's a common node (two segments are connected)
+	IF NameNode THEN
 		
+		-- find the common_node
 		SELECT (t1.from_intersection_id=t2.from_intersection_id)::int*t1.from_intersection_id +
 				(t1.from_intersection_id=t2.to_intersection_id)::int*t1.from_intersection_id +
 				(t1.to_intersection_id=t2.from_intersection_id)::int*t1.to_intersection_id +
@@ -24,9 +23,12 @@ BEGIN
 		FROM prj_volume.centreline t1, prj_volume.centreline t2
 		WHERE t1.centreline_id = c1 AND t2.centreline_id = c2;
 		
+		-- check if a collector and above segment also intersects at the common node. yes->return false; no->return true.
 		SELECT NOT EXISTS(SELECT centreline_id
 				FROM prj_volume.centreline
 				WHERE (from_intersection_id = common_node OR to_intersection_id = common_node) AND (feature_code < 201401) AND (centreline_id not in (c1,c2))) INTO result;
+				
+	-- return false if the segments do not share a common node
 	ELSE
 		result := False;
 	END IF;
