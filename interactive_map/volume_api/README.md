9da0bdc15e5ac46c3a83f07eb6edde39f62e0daf
Raphael Dumas <raphael.a.dumas@gmail.com>
2017-09-18 17:54:42 -0400

    Implement hug api for volumes

---
+++ interactive_map/volume_api/README.md
@@ -0,0 +1,5 @@
+## Volume API
+
+Testing out Python API libraries to serve data from database.
+
+[Install hug](http://www.hug.rest/website/quickstart) and then run with `hug -f get_volume_link_api.py`
---
+++ interactive_map/volume_api/get_volume_link_api.py
@@ -0,0 +1,28 @@
+import configparser 
+from psycopg2 import connect
+from psycopg2.extras import RealDictCursor 
+import psycopg2.sql as pgsql
+import hug
+# import os
+# os.chdir(r'C:\Users\rdumas\Documents\GitHub\bdit_volumes\interactive_map\volume_api')
+
+
+
+CONFIG = configparser.ConfigParser()
+CONFIG.read('../../db.cfg')
+dbsettings = CONFIG['DBSETTINGS']
+
+sql = pgsql.SQL('SELECT year, hh, volume FROM prj_volume.centreline_hourly_group12 WHERE year = {year} AND centreline_id = {centreline_id}')
+
+@hug.get(examples='year=2015&centreline_id=3154251')
+def get_volume_year_link(year: hug.types.number, centreline_id: hug.types.number):
+    '''Return 24hr volume profile for a given year and centreline_id'''
+    con = connect(**dbsettings)
+    with con.cursor(cursor_factory=RealDictCursor) as cur:
+        cur.execute(sql.format(year=pgsql.Literal(year),
+                               centreline_id=pgsql.Literal(centreline_id)))
+        data = cur.fetchall()
+    con.close()
+    return data
+
+    
