fbe422db103e00a6dfb07e6064a72330d3f01da9
Raphael Dumas <raphael.a.dumas@gmail.com>
2017-09-19 14:22:51 -0400

    Fix #58
    Return {'hh': [], 'volume': []}

--- interactive_map/volume_api/get_volume_link_api.py
+++ interactive_map/volume_api/get_volume_link_api.py
@@ -12,7 +12,7 @@ CONFIG = configparser.ConfigParser()
 CONFIG.read('../../db.cfg')
 dbsettings = CONFIG['DBSETTINGS']
 
-sql = pgsql.SQL('SELECT year, hh, volume FROM prj_volume.centreline_hourly_group12 WHERE year = {year} AND centreline_id = {centreline_id}')
+sql = pgsql.SQL('SELECT hh, volume FROM prj_volume.centreline_hourly_group12 WHERE year = {year} AND centreline_id = {centreline_id}')
 
 @hug.get(examples='year=2015&centreline_id=3154251')
 def get_volume_year_link(year: hug.types.number, centreline_id: hug.types.number):
@@ -23,6 +23,10 @@ def get_volume_year_link(year: hug.types.number, centreline_id: hug.types.number
                                centreline_id=pgsql.Literal(centreline_id)))
         data = cur.fetchall()
     con.close()
-    return data
+    data_dict = {'hh': [], 'volume': []}
+    for row in data:
+        data_dict['hh'].append(row['hh'])
+        data_dict['volume'].append(row['volume'])
+    return data_dict
 
     
