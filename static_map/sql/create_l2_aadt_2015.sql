fa7dabf2d737df0f0b317998d4d37b0ed4791de8
q-schen <31662227+q-schen@users.noreply.github.com>
2017-10-11 12:00:43 -0400

    Added SQL queries for static mapping

---
+++ static_map/sql/create_l2_aadt_2015.sql
@@ -0,0 +1,19 @@
+﻿DROP TABLE l2_aadt_2015;
+
+create table prj_volume.l2_aadt_2015 as
+(
+	select
+	cgl2.l2_group_number,
+	cgg.linear_name_full,
+	cgg.feature_code_desc fcode_desc,
+	a.dir_bin,
+	a.year,
+	round(avg(a.volume),0) avg_vol
+	, geoml2.geom
+	from prj_volume.aadt a
+	inner join prj_volume.centreline_groups_l2 cgl2 on (a.group_number = cgl2.l1_group_number)
+	inner join prj_volume.centreline_groups_geom cgg using (group_number)
+	inner join prj_volume.centreline_groups_l2_geom geoml2 using (l2_group_number)
+	group by cgl2.l2_group_number, cgg.linear_name_full, cgg.feature_code_desc, a.dir_bin, a.year, geoml2.geom
+	order by l2_group_number
+);
---
+++ static_map/sql/pythonSQLquery.sql
@@ -0,0 +1,5 @@
+﻿SELECT l2_group_number, linear_name_full, fcode_desc, dir_bin, year, avg_vol, st_transform(geom,26917) as geom
+        FROM qchen.l2_aadt_2015
+        WHERE year = {year} 
+        AND (fcode_desc IN ('Expressway','Major Arterial','Minor Arterial')
+        OR linear_name_full IN ('Finch Ave E','Old Finch Ave','McNicoll Ave','Neilson Rd','Morningside Ave','Staines Rd','Sewell''s Rd','Meadowvale Rd','Plug Hat Rd','Beare Rd','Reesor Rd'))
